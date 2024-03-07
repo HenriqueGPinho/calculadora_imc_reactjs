8dc992f1fec210c11d6cf48b9221024739d54f8b
Henrique Pinho <henriquegravino@outlook.com>
2024-03-07 10:27:50 -0300

    renderização condicional

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,13 +1,20 @@
 import styles from './Result.module.css'
 
 export default function Result({ valorIMC, classificacao }) {
-  return (
-    <div className="container">
-      <div className={styles.results}>
-        <span className={styles.result}>O seu IMC calculado é de <b>{valorIMC}</b></span>
-        <span className={styles.result}>De acordo com a tabela de referência, seu IMC é classificado como <b>{classificacao}</b></span>
+  if (!valorIMC) {
+    return (
+      <div className="container">
+          <span className={styles.result}>O seu resultado aparecerá aqui.</span>
+      </div> 
+    )
+  } else {
+    return (
+      <div className="container">
+        <div className={styles.results}>
+          <span className={styles.result}>O seu IMC calculado é de <b>{valorIMC}</b></span>
+          <span className={styles.result}>De acordo com a tabela de referência, seu IMC é classificado como <b>{classificacao}</b></span>
+        </div>
       </div>
-    </div>
-    
-  )
+    )
+  }
 }
